419bbd9d7c3a3bd401a6944e1412ae1baeccdce7
joaquincollazo84 <joaquinantoniocollazo@gmail.com>
2016-04-28 01:24:22 -0300

    Juego28/04/2016

--- tateti.py
+++ tateti.py
@@ -4,14 +4,19 @@ while b=="si":
 	x = int(raw_input(("Ingresar un numero : ")))
 	if a[x]!="x":
 		a[x]="x"
-		print a[0],a[1],a[2]
-		print a[3],a[4],a[5]
-		print a[6],a[7],a[8]
+		print " "
+		print "   -----"
+		print "  ",a[0],a[1],a[2]
+		print "  ",a[3],a[4],a[5]
+		print "  ",a[6],a[7],a[8]
+		print "   -----"
 	else:
 		a[x]=="x"
-		print a[0],a[1],a[2]
-		print a[3],a[4],a[5]
-		print a[6],a[7],a[8]
+		print "    -----"
+		print "   ",a[0],a[1],a[2]
+		print "   ",a[3],a[4],a[5]
+		print "   ",a[6],a[7],a[8]
+		print "    -----"
 		
 	b = raw_input("quiere segui si o no")
 
